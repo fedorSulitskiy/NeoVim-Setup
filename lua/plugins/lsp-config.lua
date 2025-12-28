981d52fbda083b707a441262edce2f4355c335a7
fedorSulitskiy <f.sulitskiy@gmail.com>
2025-12-28 12:53:30 +0000

    Feat: go autoimport with null ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,6 +20,7 @@ return {
 			vim.diagnostic.config(opts.diagnostics)
 
 			local lspconfig = require("lspconfig")
+			local util = require("lspconfig/util")
 			local cmp_nvim_lsp = require("cmp_nvim_lsp")
 			local capabilities = cmp_nvim_lsp.default_capabilities()
 
@@ -32,12 +33,56 @@ return {
 				},
 			}
 
-			local servers = { lua_ls = {}, gopls = {}, nixd = nixdSetup }
+			local goplsSetup = {
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotml" },
+				root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+				settings = {
+					gopls = {
+						completeUnimported = true,
+					},
+				},
+			}
+
+			local servers = { lua_ls = {}, gopls = goplsSetup, nixd = nixdSetup }
 
 			for name, server_opts in pairs(servers) do
 				server_opts.capabilities = capabilities
 				lspconfig[name].setup(server_opts)
 			end
+		end,
+	},
+
+	-- 3. null-ls for go
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		ft = "go",
+		config = function()
+			local null_ls = require("null-ls")
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.gofumpt,
+					null_ls.builtins.formatting.goimports_reviser,
+					null_ls.builtins.formatting.golines,
+				},
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({
+							group = augroup,
+							buffer = bufnr,
+						})
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ bufnr = bufnr })
+							end,
+						})
+					end
+				end,
+			})
 		end,
 	},
 }
